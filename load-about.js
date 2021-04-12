0cdce3f401208f6f5356f5a2f64eac929b242109
Abe Park <abepark26@gmail.com>
2021-04-12 08:12:25 -0400

    path fixes

--- load-about.js
+++ load-about.js
@@ -1,14 +1,14 @@
 window.onload=function() {
-    $("#about-container").load("/abepark26.github.io/about.html");
+    $("#about-container").load("./about.html");
     $("#project-container").empty();
 }
 
 function load_about() {
-    $("#about-container").load("/abepark26.github.io/about.html");
+    $("#about-container").load("./about.html");
     $("#project-container").empty();
 }
 
 function load_project() {
     $("#about-container").empty();
-    $("#project-container").load("/abepark26.github.io/project.html");
+    $("#project-container").load("./project.html");
 }
